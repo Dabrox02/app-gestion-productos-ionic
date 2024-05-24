1c60902d07b4427ca0e7a20da0097bdbff1e4fda
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2024-05-24 15:48:35 -0500

    feat: :sparkles: initial structure

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-gestion-productos-ionic',
+  webDir: 'www'
+};
+
+export default config;
